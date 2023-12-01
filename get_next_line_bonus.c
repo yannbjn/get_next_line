8605b9b7537d93a99fcbbdda6c3822f440199288
Yann Bejani <yabejani@bess-f1r4s3.clusters.42paris.fr>
2023-12-01 13:31:15 +0100

    updatebonus

---
+++ get_next_line_bonus.c
@@ -0,0 +1,118 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yabejani <yabejani@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/01 12:30:15 by yabejani          #+#    #+#             */
+/*   Updated: 2023/12/01 13:06:23 by yabejani         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+char	*ft_read(int fd, char *save)
+{
+	char	*tmp;
+	int		readbytes;
+
+	tmp = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!tmp)
+		return (NULL);
+	readbytes = 1;
+	while (!ft_strchr(save, '\n') && readbytes != 0)
+	{
+		readbytes = read(fd, tmp, BUFFER_SIZE);
+		if (readbytes == -1)
+		{
+			free(tmp);
+			tmp = NULL;
+			return (NULL);
+		}
+		tmp[readbytes] = '\0';
+		save = ft_strjoin(save, tmp);
+	}
+	free(tmp);
+	return (save);
+}
+
+char	*ft_get_line(char *save)
+{
+	char	*line;
+	int		i;
+
+	i = 0;
+	if (!save[i])
+		return (NULL);
+	while (save[i] && save[i] != '\n')
+		i++;
+	line = malloc(sizeof(char) * (i + 2));
+	if (!line)
+		return (NULL);
+	i = 0;
+	while (save[i] && save[i] != '\n')
+	{
+		line[i] = save[i];
+		i++;
+	}
+	if (save[i] == '\n')
+	{
+		line[i] = save[i];
+		i++;
+	}
+	line[i] = '\0';
+	return (line);
+}
+
+char	*ft_clean_save(char *save)
+{
+	char	*newsave;
+	int		i;
+	int		j;
+
+	i = 0;
+	while (save[i] && save[i] != '\n')
+		i++;
+	if (!save[i])
+	{
+		free(save);
+		save = NULL;
+		return (NULL);
+	}
+	newsave = malloc(sizeof(char) * (ft_strlen(save) - i + 1));
+	if (!newsave)
+		return (NULL);
+	i++;
+	j = 0;
+	while (save[i])
+		newsave[j++] = save[i++];
+	newsave[j] = '\0';
+	free(save);
+	return (newsave);
+}
+
+char	*get_next_line(int fd)
+{
+	char		*line;
+	static char	*save[4095];
+
+	if (BUFFER_SIZE <= 0 || fd < 0)
+		return (0);
+	save[fd] = ft_read(fd, save[fd]);
+	if (!save[fd])
+		return (NULL);
+	line = ft_get_line(save[fd]);
+	if (!line || !line[0])
+	{
+		free(line);
+		return (NULL);
+	}
+	save[fd] = ft_clean_save(save[fd]);
+	if (!save[fd] || save[fd][0] == 0)
+	{
+		free(save[fd]);
+		save[fd] = NULL;
+	}
+	return (line);
+}
---
+++ get_next_line_bonus.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yabejani <yabejani@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/01 12:18:49 by yabejani          #+#    #+#             */
+/*   Updated: 2023/12/01 13:04:41 by yabejani         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+size_t	ft_strlen(char *str);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strchr(char *st, int c);
+
+char	*get_next_line(int fd);
+char	*ft_get_line(char *save);
+char	*ft_clean_save(char *save);
+char	*ft_read(int fd, char *save);
+
+#endif
