02d4e92da07ac330dd6b77752a32de8a8d41fb47
Yann Bejani <yabejani@bess-f1r5s4.clusters.42paris.fr>
2023-12-04 14:22:58 +0100

    bible

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: yabejani <yabejani@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 12:22:33 by yabejani          #+#    #+#             */
-/*   Updated: 2023/12/04 11:24:14 by yabejani         ###   ########.fr       */
+/*   Updated: 2023/12/04 14:21:40 by yabejani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ char	*ft_read(int fd, char *save)
 {
 	char	*tmp;
 	int		readbytes;
-
+	
 	tmp = malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	if (!tmp)
 		return (NULL);
@@ -94,8 +94,6 @@ char	*ft_clean_save(char *save)
 	return (newsave);
 }
 
-#include <stdio.h>
-
 char	*get_next_line(int fd)
 {
 	static char	*save;
@@ -104,7 +102,6 @@ char	*get_next_line(int fd)
 	if (BUFFER_SIZE <= 0 || fd < 0)
 		return (0);
 	save = ft_read(fd, save);
-	printf("SAVE de ft_read :%s", save);
 	if (!save)
 		return (NULL);
 	line = ft_get_line(save);
@@ -113,14 +110,12 @@ char	*get_next_line(int fd)
 		free(line);
 		return (NULL);
 	}
-	printf("LINE de get line :%s", line);
 	save = ft_clean_save(save);
 	if (!save || save[0] == 0)
 		{
 			free(save);
 			save = NULL;
 		}
-	printf("SAVE de clean save :%s", save);
 	return (line);
 }
 
@@ -133,9 +128,9 @@ int	main(void)
 	int	fd1;
 	int	i;
 
-	fd1 = open("tests/test.txt", O_RDONLY);
+	fd1 = open("tests/bible.txt", O_RDONLY);
 	i = 1;
-	while (i < 6)
+	while (i < 100183)
 	{
 		line = get_next_line(fd1);
 		printf("line %d :%s", i, line);
